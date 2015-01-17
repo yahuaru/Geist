33a6e956fea535f53095e7635e90c4371339a80b
Zo0MER <exzo0mex@gmail.com>
2015-01-17 15:21:02 +0400

    Special thanks on titles was added.

--- Assets/Scripts/Menu/FinalTitle.cs
+++ Assets/Scripts/Menu/FinalTitle.cs
@@ -7,6 +7,7 @@ public class FinalTitle : MonoBehaviour {
 	public GameObject chararcter; 	
 	public SpriteRenderer  sprite1;
 	public SpriteRenderer  sprite2;
+    public SpriteRenderer specialThanks;
 	public float opacitySpeed = 5;
 
 
@@ -14,18 +15,25 @@ public class FinalTitle : MonoBehaviour {
 	private bool becomeNotOpacity;
 
 
-	void OnTriggerEnter2D(Collider2D collider){
-		if(collider.tag =="Player"){
+	void OnTriggerEnter2D(Collider2D collider)
+    {
+		if(collider.tag =="Player")
+        {
             chararcter.GetComponent<Character>().runSpeed = 0;
 			becomeNotOpacity = true;
 			tempColor = sprite1.color;
 		}
 	}
+
 	void FixedUpdate() {
 		if(becomeNotOpacity){
 			tempColor.a +=(Time.deltaTime*opacitySpeed/100); 
 			sprite1.color = tempColor;
 			sprite2.color = tempColor;
+
+            tempColor = specialThanks.color;;
+		    tempColor.a = Time.deltaTime*opacitySpeed/100;
+		    specialThanks.color = tempColor;
 		}
 	}
 }
